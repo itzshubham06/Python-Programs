01b1a22a7eaef4f05f9f34f1dced4894787c874c
Shubham Kale <shubhamkale9606@gmail.com>
2026-01-26 21:18:11 +0530

    Add files via upload

---
+++ prime_Number.py
@@ -0,0 +1,12 @@
+start = int(input("Starting range: "))
+end = int(input("Ending range: "))
+
+for num in range(start, end + 1):
+    if num > 1:
+        is_prime = True
+        for i in range(2, num):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            print(num)
